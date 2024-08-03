52da28f838fd9469e3ae010043b0e638fce3efbe
McFlurinivas <sharinivas04@gmail.com>
2024-08-03 14:09:23 +0530

    Remove unused dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:travelio/view_model/device_view_model.dart';
-import 'package:travelio/view_model/room_view_model.dart';
-//import 'package:travelio/views/home_page.dart';
 import 'package:travelio/views/splash_screen.dart';
 
-void main() {
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => DeviceViewModel()),
-        ChangeNotifierProvider(create: (_) => RoomViewModel()),
-      ],
-      child: const MyApp(),
-    ),
-  );
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,8 +1,8 @@
+
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:travelio/view_model/device_view_model.dart';
 import 'package:travelio/view_model/room_view_model.dart';
-import 'room_page.dart';
+import '../widget/device_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,26 +12,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final DeviceViewModel deviceViewModel = DeviceViewModel();
+  final RoomViewModel roomViewModel = RoomViewModel();
   bool isCelsius = true;
   final PageController _pageController = PageController();
+  final PageStorageBucket _bucket = PageStorageBucket();
+  int _currentRoomIndex = 0;
 
-  double getTemperature(double temp, bool isCelsius) {
-    return isCelsius ? temp : temp * 1.8 + 32;
-  }
+  double get temperature => isCelsius
+      ? deviceViewModel.currentTemperature
+      : deviceViewModel.currentTemperature * 1.8 + 32;
 
-  void _showAddRoomDialog(BuildContext context) {
+  void _showAddRoomDialog() {
     TextEditingController roomNameController = TextEditingController();
-    final deviceViewModel = Provider.of<DeviceViewModel>(context, listen: false);
-    final roomViewModel = Provider.of<RoomViewModel>(context, listen: false);
 
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
           backgroundColor: deviceViewModel
-              .devices[deviceViewModel.activeDeviceIndex]
-              .backgroundColor
-              .withOpacity(0.8),
+          .devices[deviceViewModel.activeDeviceIndex].backgroundColor.withOpacity(0.8),
           title: const Text('Create New Room'),
           content: TextField(
             controller: roomNameController,
@@ -47,9 +47,10 @@ class _HomePageState extends State<HomePage> {
             ElevatedButton(
               child: const Text('Create'),
               onPressed: () {
-                roomViewModel.addRoom(
-                  "${roomNameController.text[0].toUpperCase()}${roomNameController.text.substring(1).toLowerCase()}",
-                );
+                setState(() {
+                  roomViewModel.addRoom(
+                      "${roomNameController.text[0].toUpperCase()}${roomNameController.text.substring(1).toLowerCase()}");
+                });
                 Navigator.of(context).pop();
 
                 Future.delayed(const Duration(milliseconds: 200), () {
@@ -65,28 +66,204 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final deviceViewModel = Provider.of<DeviceViewModel>(context);
-    final roomViewModel = Provider.of<RoomViewModel>(context);
-
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: deviceViewModel
-          .devices[deviceViewModel.activeDeviceIndex]
-          .backgroundColor,
+          .devices[deviceViewModel.activeDeviceIndex].backgroundColor,
       body: PageView.builder(
+        key: const PageStorageKey<String>('pageView'),
         controller: _pageController,
         itemCount: roomViewModel.rooms.length,
+        onPageChanged: (index) {
+          setState(() {
+            roomViewModel.setActiveRoom(index);
+            _currentRoomIndex = index;
+          });
+        },
         itemBuilder: (context, index) {
-          return RoomPage(
-            key: ValueKey(index),
-            roomIndex: index,
-            isCelsius: isCelsius,
-            toggleTemperatureUnit: () {
-              setState(() {
-                isCelsius = !isCelsius;
-              });
-            },
-            showAddRoomDialog: _showAddRoomDialog,
+          return PageStorage(
+            key: PageStorageKey<int>(index),
+            bucket: _bucket,
+            child: AnimatedContainer(
+              duration: const Duration(seconds: 2),
+              color: deviceViewModel.devices[deviceViewModel.activeDeviceIndex]
+                      .isActive
+                  ? deviceViewModel
+                      .devices[deviceViewModel.activeDeviceIndex]
+                      .backgroundColor
+                  : Colors.black.withOpacity(0.5),
+              child: Stack(
+                children: [
+                  AnimatedSwitcher(
+                    duration: const Duration(seconds: 1),
+                    child: Image.asset(
+                      deviceViewModel.activeBackground,
+                      color: deviceViewModel
+                              .devices[deviceViewModel.activeDeviceIndex]
+                              .isActive
+                          ? null
+                          : Colors.black.withOpacity(0.5),
+                      key: ValueKey<String>(deviceViewModel.activeBackground),
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                      height: double.infinity,
+                    ),
+                  ),
+                  SafeArea(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(height: 40,),
+                          const Row(
+                            children: [
+                              Icon(Icons.arrow_back,
+                                  size: 30, color: Colors.white),
+                              Spacer(),
+                              Icon(Icons.settings,
+                                  size: 30, color: Colors.white),
+                            ],
+                          ),
+                          const SizedBox(height: 16),
+                          Text(
+                            roomViewModel.rooms[roomViewModel.activeRoomIndex].name,
+                            style: const TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          roomViewModel.rooms.length > 1
+                              ? Row(
+                                  children: roomViewModel.rooms.map((room) {
+                                    int index = roomViewModel.rooms.indexOf(room);
+                                    return Container(
+                                      width: 8.0,
+                                      height: 8.0,
+                                      margin: const EdgeInsets.symmetric(
+                                          vertical: 10.0, horizontal: 2.0),
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: _currentRoomIndex == index
+                                            ? Colors.white
+                                            : Colors.grey,
+                                      ),
+                                    );
+                                  }).toList(),
+                                )
+                              : const SizedBox(),
+                          Text(
+                            '${deviceViewModel.activeDevicesCount} devices active',
+                            style: TextStyle(
+                                fontSize: 16, color: Colors.grey[300]),
+                          ),
+                          const SizedBox(height: 16),
+                          GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                isCelsius = !isCelsius;
+                              });
+                            },
+                            child: Container(
+                              padding: const EdgeInsets.all(5),
+                              decoration: BoxDecoration(
+                                color: Colors.orange,
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              child: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  const Icon(Icons.thermostat_outlined,
+                                      color: Colors.white),
+                                  RichText(
+                                      text: TextSpan(children: [
+                                    TextSpan(
+                                      text: temperature.toInt().toString(),
+                                      style: const TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    TextSpan(
+                                      text: isCelsius ? '°c' : '°f',
+                                      style: const TextStyle(
+                                        fontSize: 16,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ]))
+                                ],
+                              ),
+                            ),
+                          ),
+                          const Spacer(),
+                          Slider(
+                            activeColor: deviceViewModel
+                                .devices[deviceViewModel.activeDeviceIndex]
+                                .backgroundColor,
+                            thumbColor: Colors.white,
+                            label: temperature.toInt().toString(),
+                            value: deviceViewModel.currentTemperature,
+                            min: 0.0,
+                            max: 100.0,
+                            onChanged: (value) {
+                              setState(() {
+                                deviceViewModel.updateTemperature(value);
+                              });
+                            },
+                          ),
+                          SizedBox(
+                            height: 150,
+                            child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: deviceViewModel.devices.length,
+                              itemBuilder: (context, index) {
+                                return Padding(
+                                  padding: const EdgeInsets.only(right: 16.0),
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        if (deviceViewModel.activeDeviceIndex ==
+                                            index) {
+                                          deviceViewModel
+                                              .toggleDeviceStatus(index);
+                                        } else {
+                                          deviceViewModel
+                                              .setActiveDevice(index);
+                                        }
+                                      });
+                                    },
+                                    child: SizedBox(
+                                        width: 140,
+                                        child: DeviceWidget(
+                                            device: deviceViewModel
+                                                .devices[index])),
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          InkWell(
+                            onTap: _showAddRoomDialog,
+                            child: const Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(Icons.add_circle_outline,
+                                      size: 30, color: Colors.white),
+                                Text('Add', style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700)),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           );
         },
       ),
